00e155a67cc331e93bc388da14e67b99ccfac6b2
Caio LK <caio.kozano@live.com>
2022-03-03 00:20:43 -0300

    Adição de rotas fornecedores, clientes, produtos e controllers + fallback

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{ PrincipalController, 
-                           SobreNosController, 
-                           ContatoController,
-                          FornecedorController };
+use App\Http\Controllers\{ PrincipalController, SobreNosController, ContatoController,
+                           FornecedorController, ClienteController, ProdutoController };
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,12 +14,17 @@ use App\Http\Controllers\{ PrincipalController,
 |
 */
 
-Route::get('/','PrincipalController@index')->name('site.index');
-Route::get('/sobre-nos','SobreNosController@index')->name('site.sobrenos');
-Route::get('/contato','ContatoController@index')->name('site.contato');
+Route::get('/', [PrincipalController::class, 'index'])->name('site.index');
+Route::get('/sobre-nos', [SobreNosController::class, 'index'])->name('site.sobrenos');
+Route::get('/contato',[ContatoController::class, 'index'])->name('site.contato');
 Route::get('/login', function () { return 'login'; })->name('site.login');
 
 Route::group(['prefix' => 'app'], function() {
-    
+    Route::get('/clientes', [ClienteController::class, 'index'])->name('app.clientes');
+    Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
+    Route::get('/produtos', [ProdutoController::class, 'index'])->name('app.produtos');
 });
 
+Route::fallback( function(){
+    return 'Rota nao encontrada! Clique <a href="'.route('site.index').'">aqui</a> para retornar para a página incial!';
+});
